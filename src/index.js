6066932aa4c08f70e7440ddb221a3958c9011d3f
SiarheiKuchko <138041905+SiarheiKuchko@users.noreply.github.com>
2023-10-29 21:39:03 +0200

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  str = String(Math.abs(n));
+      m = str.length-1;
+      strNew = '';
+       for (i=0; i < str.length; i++){
+        strNew = strNew + str[m];
+        m = m - 1;
+      }
+      num = Number(strNew);
+      return num;
 }
